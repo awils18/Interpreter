836a7ed3630bac74c32df07188d5d00854f828de
Yaron Shahverdi <yaronshahverdi@dhcp-10-5-9-170.wireless.rochester.edu>
2014-11-21 22:42:22 -0500

    edited grammar

--- parser.py
+++ parser.py
@@ -19,13 +19,14 @@ class Parser():
             print('PARSING ERROR -- UNEXPECTED TOKEN "%s"' % self.input_token.value)
         sys.exit(1)
 
-    def match(self, expected_token, func):
-        if self.input_token.value == expected_token:
-            if self.debug:
-                print("MATCHED '%s' from %s" % (self.input_token.value,func))
-            self.consume_token()
-        else:
-            self.parse_error(func)
+    def match(self, *args, **kwargs):
+        for expected_token in args:
+            if self.input_token.value == expected_token:
+                if self.debug:
+                    print("MATCHED '%s' from %s" % (self.input_token.value,kwargs['func']))
+                self.consume_token()
+            else:
+                self.parse_error(kwargs['func'])
 
     def match_type(self, expected_type, func):
         if self.input_token.type == expected_type:
@@ -35,21 +36,62 @@ class Parser():
         else:
             self.parse_error(func)
 
+    def fLambda(self):
+        self.match('(','lambda')
+        self.params()
+        self.expression()
+        self.match(')')
+
+    def params(self):
+        self.match('(')
+        self.match_type('ID')
+        self.params()
+        self.match(')')
+
+    def params_list(self):
+        if self.input_token.type == 'ID':
+            self.match_type('ID')
+            self.params_list()
+        elif self.input_token.value == ')':
+            pass
+        else:
+            self.parse_error('params_list')
+
+    def expression(self):
+        if self.input_token.type == 'ID':
+            self.match_type('ID')
+        elif self.input_token.value == '(':
+            self.match('(')
+            self.operator()
+            self.expression()
+            self.expression()
+            self.match(')')
+
+    def operator(self):
+        if self.input_token.value in ['+','-','*','/']:
+            self.match(self.input_token.value)
+        else:
+            self.parse_error('operator')
+
+    def application(self):
+        pass
+
+    def head(self):
+        pass
+
+    def tail(self):
+        pass
+
+
+    
+
+
+
 
 
 file = sys.argv[1]
 print("PARSING FOR " + file)
-#p = Parser(file)
-t = Tokenizer(file)
-token = t.next_token()
-while token.value != '$$':
-    print(token.value + ':' + token.type)
-    token = t.next_token()
-
-
-
-
-
+p = Parser(file)
 
 
 
