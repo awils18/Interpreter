59b90aa22040ae66ac51d81bcb287e34d384addc
awils18 <awils18@u.rochester.edu>
2014-11-23 16:17:53 -0500

    Basic evaluation of ops

--- evaluator.py
+++ evaluator.py
@@ -11,25 +11,53 @@ class Evaluator():
         for child in M:
             print child.value
         print "####################"'''
+      
+        
+        # The case of an empty array
         if len(M) == 0:
             return []
-        if M[0].type in ["number", "ID"] and len(M) == 1:
+        
+        # The case of id, number, or op
+        elif M[0].type in ["number", "ID", "operator"] and len(M) == 1:
             #print "In ID NUM 2"
             return [Node(M[0].type, M[0].value)]
+    
+        # The case of + 5 5) operations
+        elif M[0].type == "operator":
+            if M[1].type == "number" and M[2].type == "number":
+                return [Node("number", (int(M[1].value) + int(M[2].value)))]
+        
+        # The case of (lambda (x) x)
         elif M[1].value == "lambda":
             return [Node(M[3].type, M[3].value)]
+        
+        # The case of lambda (x) expr) application
+        elif M[0].value == "lambda":
+            v = self.evalNodeArray([M[4]])[0]
+            f = self.evalNodeArray([M[2]])[0]
+            self.env[f.value] = v.value
+            return [Node("number", v.value)]
+        
+        # The case of (5 after an evaluation
+        elif len(M) > 1 and M[0].value == "(" and M[1].type == "number":
+            return [Node(M[1].type, M[1].value)]
+        
+        # The case of (head tail) application
         elif M[1].value == "head" and M[2].value == "tail":
-            v = self.evalNodeArray(M[2])
-            f = self.evalNodeArray(M[1])
-            env[f] = v
-            return [Node("number", v)]
-        elif M[1].type in ["number", "ID"] and M[2].type in ["number", "ID"]:
+            v = self.evalNodeArray([M[2]])[0]
+            f = self.evalNodeArray([M[1]])[0]
+            self.env[f.value] = v.value
+            return [Node("number", v.value)]    
+        
+        # The case of (x 5) simple application
+        elif len(M) > 2 and M[1].type in ["number", "ID"] and M[2].type in ["number", "ID"]:
             v = self.evalNodeArray([M[2]])[0]
             f = self.evalNodeArray([M[1]])[0]
             self.env[f.value] = v.value
             return [Node("number", v.value)]         
+       
+        # The case of meaningless characters to be appended
         else:
-           # print "FUCKs"
             return M
 
     def processTree(self, node):
