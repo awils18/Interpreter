60443f03e815e6affa179426769354458be6718c
awils18 <awils18@u.rochester.edu>
2014-11-23 15:36:16 -0500

    Recursive cap

--- evaluator.py
+++ evaluator.py
@@ -3,40 +3,6 @@ from ASTparser import *
 class Evaluator():
     
     env = {}
-    mArray = ['','']
-    
-    # Return true if x is a function (Lx.x)
-    def isFunc(self, x):
-        #print x
-        if x[1].type == "Lambda":
-            return True
-        return False
-    
-    # Seperate the str into (M1 M2)
-    # Access in the mArray[] = {M1, M2}
-    def seperateApplication(x):
-        str1 = ''
-        str2 = ''
-        
-        # Is the first str being processed
-        isFirst = True
-        
-        for n in range(1, len(x)-1):
-            if x[n] == ' ':
-                pass
-            elif x[n] != ')':
-                if isFirst:
-                    str1 += x[n]
-                else:
-                    str2 += x[n]
-            else:
-                if isFirst:
-                    isFirst = False
-                    str1 += x[n]
-                else:
-                    str2 += x[n]
-        mArray[0] = str1
-        mArray[1] = str2
     
     # Evaluate the expression the Lambda function M and return reduction
     def evalNodeArray(self, M):
