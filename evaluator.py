914189d5529cac322dd2bbe753445757e17fdd05
awils18 <awils18@u.rochester.edu>
2014-11-23 14:19:30 -0500

    Fixed basic applications

--- evaluator.py
+++ evaluator.py
@@ -41,18 +41,27 @@ class Evaluator():
     # Evaluate the expression the Lambda function M and return reduction
     def evalNodeArray(self, M):
         
+        '''print "##################"
+        for child in M:
+            print child.value
+        print "####################"'''
         if len(M) == 0:
             return []
-        if M[0].type in ["number", "id"]:
+        if M[0].type in ["number", "ID"] and len(M) == 1:
+            #print "In ID NUM 2"
             return [Node(M[0].type, M[0].value)]
         elif M[1].value == "lambda":
             return [Node(M[3].type, M[3].value)]
         elif M[1].value == "head" and M[2].value == "tail":
-            print "FUCKKKASDAD"
             v = self.evalNodeArray(M[2])
             f = self.evalNodeArray(M[1])
             env[f] = v
             return [Node("number", v)]
+        elif M[1].type in ["number", "ID"] and M[2].type in ["number", "ID"]:
+            v = self.evalNodeArray([M[2]])[0]
+            f = self.evalNodeArray([M[1]])[0]
+            self.env[f.value] = v.value
+            return [Node("number", v.value)]         
         else:
            # print "FUCKs"
             return M
@@ -75,11 +84,11 @@ class Evaluator():
 parser = ASTParser("testlambda.txt")
 evaluator = Evaluator()
 for tree in parser.expression_trees:
-    
-    for child in evaluator.env.keys():
-        print child
+    #for child in evaluator.env.keys():
+        #print child
     print "EVAL: " 
-    print evaluator.processTree(tree)[2].value
+    evaluation = evaluator.processTree(tree)[0]
+    print evaluation.value
 
     
     
